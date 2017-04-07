a17bcf497ddf5909c26da47d93862980546a8a88
gauravshetti <gaurav.shetti@atscale.com>
2017-04-07 15:48:06 -0700

    simplifying

--- src/common_functions.js
+++ src/common_functions.js
@@ -1,25 +1,24 @@
 /* 
 Version Beta 0.0.1
 */
-function test_response_code() {
-    this.json_200 = function(){
-        describe("valid json", () => {
-            it('should return a 200 response', () => {
+function test_response_200_json() {
+    describe("valid json", () => {
+        it('should return a 200 response', () => {
             response.should.have.status(200);
-            });
-            it('should return a json response', () => {
-                response.should.be.json
-            });
         });
-    };
-    this.json_400 = function(){
-        describe("invalid json", () => {
-            it('should return a 400 response', () => {
+        it('should return a json response', () => {
+            response.should.be.json
+        });
+    });
+};
+
+function test_response_400_json() {
+    describe("invalid json", () => {
+        it('should return a 400 response', () => {
             response.should.have.status(200);
-            });
-            it('should return a json response', () => {
-                response.should.be.json
-            });
         });
-    }; 
+        it('should return a json response', () => {
+            response.should.be.json
+        });
+    });
 }
