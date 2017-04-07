3dbaf04e0d4f564c2583ce769f03486906c90abb
gauravshetti <gaurav.shetti@atscale.com>
2017-04-07 16:16:48 -0700

    more common funtions

--- src/common_functions.js
+++ src/common_functions.js
@@ -12,10 +12,32 @@ function test_response_200_json() {
     });
 }
 
+function test_response_200_text() {
+    describe("valid json", () => {
+        it('should return a 200 response', () => {
+            response.should.have.status(200);
+        });
+        it('should return a json response', () => {
+            response.should.be.text;
+        });
+    });
+}
+
 function test_response_400_json() {
     describe("invalid json", () => {
         it('should return a 400 response', () => {
-            response.should.have.status(200);
+            response.should.have.status(400);
+        });
+        it('should return a json response', () => {
+            response.should.be.json;
+        });
+    });
+}
+
+function test_response_401_json() {
+    describe("invalid json", () => {
+        it('should return a 401 response', () => {
+            response.should.have.status(401);
         });
         it('should return a json response', () => {
             response.should.be.json;
